4329057f3df965187157de4d643fba8fe952e33e
Test User <TestUser@fakeemail.com>
2021-05-19 15:34:03 -0400

    Class work Chap 5

--- WIP-Walker/Ch05/part1/error-first-callback.js
+++ WIP-Walker/Ch05/part1/error-first-callback.js
@@ -1,11 +1,13 @@
 
 const fs = require("fs");
 
+const wordCount = (string) => string.split(" ").length;
 fs.readFile('input.txt', function (err, data) {
     if (err) {
         return console.error(err);
     }
     console.log('This was in the file input ', data.toString());
+    console.log('The number of words = ', wordCount(data));
 });
 
 
@@ -14,4 +16,11 @@ fs.readFile('content.txt', function(err, data){
         return console.error(err);
     }
     console.log('This was in the file Content ', data.toString());
+})
+
+fs.stat('content.txt', function(err, stats){
+    if (err) {
+        return console.error(err);
+    }
+    console.log(stats);
 })
