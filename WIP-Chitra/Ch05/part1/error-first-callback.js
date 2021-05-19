9ab1452ecf237e06f73465e8f9334b82292912b6
Test User <TestUser@fakeemail.com>
2021-05-19 15:57:10 -0400

    Committing at the end of the day on 0519

---
+++ WIP-Chitra/Ch05/part1/error-first-callback.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+// Asynchronous read
+fs.readFile('./input.txt', function(err, data) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("Asynchronous read: " + data);
+});
+
+console.log('End of program file');
