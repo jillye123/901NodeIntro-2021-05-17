56323f3f6985a679e97d4ce160154d003e20fceb
Test User <TestUser@fakeemail.com>
2021-05-19 14:53:49 -0400

    exercise

---
+++ WIP-Susan/Ch04/npm-with-package/timeExample.js
@@ -0,0 +1,17 @@
+var moment = require('moment');
+console.log(moment().format('MM-DD-YYYY'));
+
+
+
+console.log(
+    moment("20110101", "YYYYMMDD").fromNow()
+);
+
+
+
+console.log(moment().dayOfYear());
+
+console.log(moment().day());
+
+console.log(moment.defaultFormat);
+console.log(moment.HTML5_FMT);
