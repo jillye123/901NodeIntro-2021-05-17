b5766025669a400f4e480bac4d3c77bb899e909b
Test User <TestUser@fakeemail.com>
2021-05-21 13:48:46 -0400

    init express

--- WIP-phillip/ch08/express1/lab-project/app.js
+++ WIP-phillip/ch08/express1/lab-project/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const moment = require('moment');
+const debug = require('debug')('lab-project:server');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    debug('in the first custom middleware');
+    let now = moment().format('MMM Do YYYY, h:mm:ss a');
+    debug(`Debug Time: ${now} and the URL is ${req.url}`);
+    next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
