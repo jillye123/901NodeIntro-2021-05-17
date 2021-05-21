a057bc491a97fa48d9c08c8a66d2a1c67bdb512f
Test User <TestUser@fakeemail.com>
2021-05-21 13:50:41 -0400

    Friday afterooon commits

--- WIP/Ch08/express1/lab-project/app.js
+++ WIP/Ch08/express1/lab-project/app.js
@@ -1,3 +1,6 @@
+const moment = require('moment');
+const debug = require('debug')('lab-project:server');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -14,7 +17,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    debug('in the first custom middleware');
+    //console.log(`req.ur`, req.url);
+    let now = moment().format('MMMM Do YYYY, h:mm:ss a');
+    debug(`Debug Time: ${now} and the URL is ${req.url}`);
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use((err, request, response, next) => {
+    //log the error, for now just console.log
+    console.log(err);
+    response.status(500).send('Something broke!');
+});
+
+app.use((req, res, next) => {
+    if (req.url === '/badrequest') {
+        next("PRETENDING TO BE A SERVER ERROR")
+    }
+    next();
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 resources not found');
+});
 
 module.exports = app;
