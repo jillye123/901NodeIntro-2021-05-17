31d1b860a0b65a71bd177b6977941e62ca90cb88
Test User <TestUser@fakeemail.com>
2021-05-21 13:40:44 -0400

    add express lab

--- WIP/Ch08/express1/lab-project/app.js
+++ WIP/Ch08/express1/lab-project/app.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const moment = require('moment');
+const debug = require('debug')('lab-project-serer');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +17,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use((req, res, next) => {
+    debug('in the first custom middleware');
+    let now = moment().format('MMMM Do YYYY, h:mm:ss a');
+    debug(`Debug Time: ${now} and the URL is ${req.url}`);
+    next();
+});
+
+app.get('/', indexRouter);
+app.get('/users', usersRouter);
+
+app.use((req, res, next) => {
+    if (req.url === '/badrequest') {
+        next("PRETENDING TO BE A SERVER ERROR");
+    }
+    next();
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 resource not found');
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send('Something broke!');
+});
 
 module.exports = app;
