8510799e9861157b5f1c768a4b71330c1859159e
Test User <TestUser@fakeemail.com>
2021-05-21 13:48:37 -0400

    latest push

--- WIP-Walker/Ch08/express1/lab-project/app.js
+++ WIP-Walker/Ch08/express1/lab-project/app.js
@@ -6,7 +6,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+
+const moment = require('moment');
+const { appendFile } = require('fs');
+const debug = require('debug')('lab-project:server');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +19,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    debug('in the first custome middleware');
+    let now = moment().format('MMMM Do YYYY, h:mm:ss a');
+    debug(`Debug Time: ${now} and the URL is ${req.url}`);
+    next();
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// app.use((req, res) => {
+//     res.status(404).send('404 resource not found');
+// });
+
+app.use((err, request, response, next) => {
+    console.log(err);
+    response.status(500).send('Something Broke!');
+    next();
+});
+app.use((req, res, next) => {
+    if (req.url === 'badrequest') {
+        next("PRETENDING TO BE A SERVER ERROR")
+    }
+    next();
+})
 
 module.exports = app;
